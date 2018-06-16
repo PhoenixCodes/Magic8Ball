f28a73da6b31fc80a491d3baf1ba5ce7c8a7a3a8
PhoenixCodes <regoldberg@gmail.com>
2018-06-15 22:19:21 -0500

    Added semicolons
    Stylistic semicolons added in

--- Magic8.js
+++ Magic8.js
@@ -12,37 +12,38 @@ shakeTimes = 4 + Math.floor(Math.random()*13)
 
 // this shows the ball in 'up' or 'down' position
 function show8Ball() {
-	console.clear()
-	if (shaking) {console.log('\n')}
+	console.clear();
+	if (shaking) {console.log('\n')};
 	console.log( '\
 \n .-"""-.\
 \n/   _   \\\
 \n\|  (8)  |\
 \n\\   ^   /\
 \n \'-...-\'\
-')
-shaking = !shaking }
+');
+shaking = !shaking };
 
 //show the ball a given number of times, then show answer
 function shakeTheBall(times) {
 	for (i = 0; i < times; i++) {
 		setTimeout(function() {
 			show8Ball();
-		} , 500*i)
-	}
+		} , 500*i);
+	};
 	setTimeout(function() {
-		console.clear()
-		console.log(getAnswer())
-	}, 500*times)
-}
+		console.clear();
+		console.log(getAnswer());
+	}, 500*times);
+};
 
 //return the answer we get
 function getAnswer() {
-	var whichAns = Math.floor(Math.random() * answers.length)
-	return answers[whichAns]
-}
+	var whichAns = Math.floor(Math.random() * answers.length);
+	return answers[whichAns] ; 
+};
 
 // "Hi, I'm running for you'
-console.log('Magic 8 ball shaking!')
+console.log('Magic 8 ball shaking!');
 // and in a bit, we will shake it
-setTimeout(function() {shakeTheBall(shakeTimes)}, 1000)
+setTimeout(function() {shakeTheBall(shakeTimes)}, 1000);
+setTimeout(function() {}, 20000);
